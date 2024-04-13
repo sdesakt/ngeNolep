d6646eb9979be8f497bcddcecf92749e1dedc3c0
Sangidealis Desak <desak.dragon@gmail.com>
2024-04-13 10:43:48 +0700

    tugas 1-3 function

---
+++ 7.function/tugas2-calcMultiply.js
@@ -0,0 +1,15 @@
+// Tugas 2
+// Buatlah sebuah fungsi bernama calculateMultiply(), yang mengembalikan nilai berupa hasil kali dari
+// dua parameter yang dikirim.
+
+//bikin fungsinya disini
+
+const calculateMultiply = (number1, number2) => {
+  return number1 * number2;
+};
+
+let num1 = 10;
+let num2 = 23;
+
+let hasilPerkalian = calculateMultiply(num1, num2);
+console.log(hasilPerkalian);
