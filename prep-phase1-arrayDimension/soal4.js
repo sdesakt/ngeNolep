126a6e847c17d6ed90fa029fe592c89df67202fb
Sangidealis Desak <desak.dragon@gmail.com>
2024-04-25 05:46:55 +0700

    update soal 4 preparation phase 1 array dimensions

--- prep-phase1-arrayDimension/soal4.js
+++ prep-phase1-arrayDimension/soal4.js
@@ -26,11 +26,23 @@ output: ['dont', 'know', 'what', 'have', 'lose']
 function averageLengthWord(words) {
   //code here
   const array = words.split(" ");
-  return array;
-  //   let sum = 0;
-  //   for (let i = 0; i < array.length; i++) {
-  //     array.
-  //   }
+  let sumWords = 0;
+  let countWord = 0;
+  for (let i = 0; i < array.length; i++) {
+    let word = array[i];
+    sumWords += word.length;
+    countWord++;
+  }
+
+  const meanWord = Math.round(sumWords / countWord);
+  let result = [];
+
+  for (let i = 0; i < array.length; i++) {
+    if (array[i].length === meanWord) {
+      result.push(array[i]);
+    }
+  }
+  return result;
 }
 
 console.log(averageLengthWord("Do you want to become a great coder ?")); // ['you']
